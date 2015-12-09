2ddbdb7a0b65b0c5c02678119912d2893a711e9a
Misha Bosin <misha@uber.com>
2015-12-09 15:52:11 -0800

    Steps 1-7

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/ContactsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/ContactsActivity.java
@@ -1,11 +1,14 @@
 package com.codepath.android.lollipopexercise.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.codepath.android.lollipopexercise.R;
 import com.codepath.android.lollipopexercise.adapters.ContactsAdapter;
@@ -16,6 +19,8 @@ import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
     private ContactsAdapter mAdapter;
+    private List<Contact> mContacts;
+    private RecyclerView rvContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,7 @@ public class ContactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacts);
 
         // Find RecyclerView and bind to adapter
-        final RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
 
         // allows for optimizations
         rvContacts.setHasFixedSize(true);
@@ -36,10 +41,10 @@ public class ContactsActivity extends AppCompatActivity {
         rvContacts.setLayoutManager(layout);
 
         // get data
-        List<Contact> contacts = Contact.getContacts();
+        mContacts = Contact.getContacts();
 
         // Create an adapter
-        mAdapter = new ContactsAdapter(ContactsActivity.this, contacts);
+        mAdapter = new ContactsAdapter(ContactsActivity.this, mContacts);
 
         // Bind adapter to list
         rvContacts.setAdapter(mAdapter);
@@ -59,6 +64,35 @@ public class ContactsActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        return super.onOptionsItemSelected(item);
+        switch (id) {
+            case R.id.miAdd:
+                addContact();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void removeContact(int pos) {
+        this.mContacts.remove(pos);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void addContact() {
+        final int pos = 0;
+        Contact conatact = Contact.getRandomContact(this);
+        this.mContacts.add(pos, conatact);
+        mAdapter.notifyDataSetChanged();
+        rvContacts.scrollToPosition(pos);
+
+        Snackbar.make(rvContacts, R.string.contact_added, Snackbar.LENGTH_LONG)
+                .setAction(R.string.undo, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        removeContact(pos);
+                    }
+                })
+                .setActionTextColor(ContextCompat.getColor(ContactsActivity.this, R.color.accent))
+                .show();
     }
 }
